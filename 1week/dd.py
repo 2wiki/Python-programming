1f6685471556699c20a0db94392b8ca27eebb72a
2wiki <41045679+2wiki@users.noreply.github.com>
2018-09-28 21:38:28 +0900

    homework
    homework

---
+++ 1week/dd.py
@@ -0,0 +1,10 @@
+Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> dd
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    dd
+NameError: name 'dd' is not defined
+>>> 1+1
+2
+>>> 
