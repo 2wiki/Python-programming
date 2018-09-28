64e10059b7c1e8929bdd5b6a5e83401becff2c75
2wiki <41045679+2wiki@users.noreply.github.com>
2018-09-28 21:04:48 +0900

    연습문제
    연습문제

---
+++ 1week/연습문제.py
@@ -0,0 +1,31 @@
+Python 3.4.2rc1 (v3.4.2rc1:8711a0951384, Sep 21 2014, 21:21:43) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 1
+1
+>>> 3+3
+6
+>>> "kmooc"
+'kmooc'
+>>> 'kmoom
+SyntaxError: EOL while scanning string literal
+>>> 
+>>> 'l
+SyntaxError: EOL while scanning string literal
+>>> 'kmooc'
+'kmooc'
+>>> "l
+SyntaxError: EOL while scanning string literal
+>>> 1+1
+2
+>>> print(:"hellw world")
+SyntaxError: invalid syntax
+>>> print("prnt hellow world")
+prnt hellow world
+>>> ================================ RESTART ================================
+>>> 
+2
+>>> print("한동대학교")
+한동대학교
+>>> print("이홍구")
+이홍구
+>>> 
