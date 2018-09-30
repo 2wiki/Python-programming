1259d5e5adea57712aaf986f747eb7d529f8f848
2wiki <41045679+2wiki@users.noreply.github.com>
2018-09-30 09:51:48 +0900

    d
    d

---
+++ 2week/연습문제2.py
@@ -0,0 +1,20 @@
+name = 'honggoo lee'
+age = 23
+print('My name is',name,',and',age,'years old.')
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+print('My name is',name,'and',age,'years old.')
+
+year = 2015
+university = '한동대학교'
+print('나는',year,'년도에 ',university,'에 입학했습니다.')
+
+number1 = float(input(3.221))  
+number2 = float(input(2.566))                
+print(number1 + number2)
+
+number = int(input(10))
+print('*'*number)
+
+
